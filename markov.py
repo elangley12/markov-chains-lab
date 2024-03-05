74b0a5adba64a8f16de729e17b455befe5c05d83
Emily <emily.j.langley@gmail.com>
2024-03-05 12:04:45 -0800

    open_and_read_file(file_path)

--- markov.py
+++ markov.py
@@ -10,9 +10,9 @@ def open_and_read_file(file_path):
     the file's contents as one string of text.
     """
 
-    # your code goes here
+    contents = open(file_path).read()
 
-    return 'Contents of your file as one long string'
+    return contents
 
 
 def make_chains(text_string):
